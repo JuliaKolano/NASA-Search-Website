8f9ef5b07996f148fe54aeb2a3546cc6e538ae29
Julia Kolano <96778608+JuliaKolano@users.noreply.github.com>
2023-03-08 13:16:34 +0000

    added loading screen and event listener

--- index.js
+++ index.js
@@ -1,50 +1,70 @@
 window.addEventListener("load", function() {
-    const form = document.querySelector("#form"),
+    const body = document.querySelector("#body")
+    form = document.querySelector("#form"),
     searchBar = document.querySelector("#searchBar"),
     searchButton = document.querySelector("#searchButton"),
     imageTarget = document.querySelector("#imageTarget"),
     prompt = document.querySelector("#prompt"),
+    loading = document.querySelector("#loading"),
+    bodyStyle = body.classList,
     formStyle = form.classList,
     searchBarStyle = searchBar.classList,
     searchButtonStyle = searchButton.classList,
-    promptStyle = prompt.classList;
+    imageTargetStyle = imageTarget.classList,
+    promptStyle = prompt.classList,
+    loadingStyle = loading.classList,
 
 
     form.addEventListener("submit", function(evt) {
         evt.preventDefault();
+        promptStyle.add("hide");
         while (imageTarget.firstChild) {
             imageTarget.removeChild(imageTarget.firstChild);
         }
         var searchInput = searchBar.value.trim();
         if (searchInput.length > 0) {
-            promptStyle.add("hide");
-            formStyle.add("formToTop");
-            searchBarStyle.add("searchBarToTop");
-            searchButtonStyle.add("buttonToTop");
+            formStyle.add("hide");
+            bodyStyle.add("noBackground")
+            loadingStyle.remove("hide");
             var xhr = new XMLHttpRequest();
             xhr.addEventListener("load", function() {
+                loadingStyle.add("hide");
+                formStyle.add("formToTop");
+                searchBarStyle.add("searchBarToTop");
+                searchButtonStyle.add("buttonToTop");
+                formStyle.remove("hide");
+                bodyStyle.remove("noBackground");
                 if (xhr.status == 200) {
                     const info = JSON.parse(xhr.responseText),
                     items = info.collection.items;
                     target = document.querySelector("#imageTarget");
                     for (let i = 0; i < items.length; i++) {
                         const img = document.createElement("img"),
-                        imgStyle = img.classList;
+                            imgStyle = img.classList;
                         imgStyle.add("image");
                         img.src = items[i].links[0].href;
                         target.append(img);
+                        img.addEventListener("click", function() {
+                            imageTargetStyle.add("hide");
+                            bodyStyle.add("blurBackground");
+                            
+                        });
+                    }
+                    if (imageTarget.childElementCount <= 0) {
+                        prompt.innerHTML = "No results";
+                        promptStyle.remove("hide");
                     }
                 }
             })
             xhr.open("GET", "https://images-api.nasa.gov/search?q=" + searchInput.trim(), true);
             xhr.send();
-            if (imageTarget.childElementCount <= 0) {
-                prompt.innerHTML = "No results";
-                promptStyle.remove("hide");
-            } 
         } else {
             prompt.innerHTML = "Please input a keyword to search";
             promptStyle.remove("hide");
         }
     })
 })
+
+// function displayDetails() {
+//     bodyStyle.add("blurBackground");
+// }
